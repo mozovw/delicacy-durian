4dd94854242bebb12c72d2741bf018a1d8e44b7c
zhangyt <v_zhangyt@fosun.com>
2019-07-29 16:55:55 +0800

    cache

---
+++ durian-cache/src/main/java/com/delicacy/durian/cache/DurianCacheApplication.java
@@ -0,0 +1,13 @@
+package com.delicacy.durian.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurianCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DurianCacheApplication.class, args);
+	}
+
+}
